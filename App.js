fee9577e15f148fc088eb11ed3bba6686221d96e
deveshkumar <devesh.dk@outlook.com>
2024-01-02 21:30:42 +0530

    episode01

---
+++ App.js
@@ -0,0 +1,7 @@
+//{} is used for attributes eg. id 
+// react.createelement is not much a javascript object.
+// render function takes this javascript object and convert it into html.
+const heading = React.createElement("h1",{id:"heading"},"Hello world from React!");
+console.log(heading);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
---
+++ Nestedhtml.js
@@ -0,0 +1,43 @@
+/**
+ * 
+ * <div id="parent">
+ *  <div id="childen1">
+ *      <h1>I am an h1 tag</h1>
+ *      <h2>I am an h2 tag</h2>
+ *  </div>
+ *  <div id="childen2s">
+ *      <h1>I am an h1 tag</h1>
+ *      <h2>I am an h2 tag</h2>
+ *  </div>
+ * </div>
+ * 
+ * To have sibling childen we can use an array of children.
+ * 
+ */
+
+
+
+const parent = React.createElement(
+    "div",
+    {id:"parent"},[
+        React.createElement(
+            "div",
+            {id:"children1"},
+            [React.createElement(
+                "h1",{id:"heading"},"I am an h1 tag."
+            ),React.createElement("h2",{id:"heading"},"I am an h2 tag.")]
+        ),
+        React.createElement(
+            "div",
+            {id:"children2"},
+            [React.createElement(
+                "h1",{id:"heading"},"I am an h1 tag."
+            ),React.createElement("h2",{id:"heading"},"I am an h2 tag.")]
+        )
+    ]
+   
+)
+
+console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(parent);
